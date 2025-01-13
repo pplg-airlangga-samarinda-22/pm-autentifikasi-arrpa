2645533fd1e95c60a6673babdb0ea59ddfed0754
arrpa <ahmadbrinegt@gmail.com>
2025-01-13 14:39:38 +0800

    selesai

---
+++ web/register.php
@@ -0,0 +1,64 @@
+<?php
+
+require "../controller/koneksi.php";
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $nik = $_POST['nik'];
+
+    $sql = "SELECT * FROM masyarakat WHERE nik=?";
+    $cek = $koneksi->execute_query($sql, [$nik]);
+
+    if (mysqli_num_rows($cek) === 1) {
+        echo "<script>alert('NIK sudah digunakan!')</script>";
+
+    } else {
+        $nama = $_POST['nama'];
+        $telepon = $_POST['telepon'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $sql = "INSERT INTO masyarakat SET nik=?, nama=?, telp=?, username=?, password=?";
+        $koneksi->execute_query($sql, [$nik, $telepon, $username, $password]);
+        echo "<script>alert('Pendaftaran berhasil!')</script>";
+        header("locatiom:login.php");
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi</title>
+</head>
+<body>
+
+    <h1>Registrasi Pengguna Baru</h1>
+    <form action="" method="post">
+        <div class="form-item">
+            <label for="nik">NIK</label>
+            <input type="text" name="nik" id="nik">
+        </div>
+        <div class="form-item">
+            <label for="name">Nama lengkap</label>
+            <input type="text" name="nama" id="nama">
+        </div>
+        <div class="form-item">
+            <label for="telepon">Telepon</label>
+            <input type="tel" name="telepon" id="telepon">
+        </div>
+        <div class="form-item">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div class="form-item">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button type="submit">Register</button>
+    </form>
+    <a href="login.php">Batal</a>
+    
+</body>
+</html>
